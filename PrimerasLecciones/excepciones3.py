e6ece9026278b351cd1e13ec3ca16f631e1d5ead
Gabriel Garcia <gggarrido@minsait.com>
2020-06-24 18:17:30 +0200

    añadido excepciones3

---
+++ PrimerasLecciones/excepciones3.py
@@ -0,0 +1,18 @@
+# el raise lanza una excepcion propia, aqui como supuestamente no se han visto POO se lanza un 
+# Typeerror que es un tipo de excepcion que se le puede pasar string de mierda
+# Vamos que el raise es igual que el throw
+
+def evaluaEdad(edad):
+    if edad < 0:
+        raise TypeError("No se permiten edades negativas")
+    if edad < 20:
+        return "eres muy joven"
+    elif edad < 40:
+        return "eres joven"
+    elif edad < 65:
+        return "eres maduro"
+    elif edad < 100:
+        "Cuidate..."
+
+edad=(int(input("Introduce tu edad: ")))
+print(evaluaEdad(edad))
