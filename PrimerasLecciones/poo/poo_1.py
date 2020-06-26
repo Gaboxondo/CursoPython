1089a4302a2c9a8d2e176aa96057df704d7e16a1
GabiMSI <gabigarciagar@gmail.com>
2020-06-26 20:21:38 +0200

    terminada clase 27

--- PrimerasLecciones/poo/poo_1.py
+++ PrimerasLecciones/poo/poo_1.py
@@ -12,14 +12,33 @@ class Coche:
     # this dado
     def arrancar(self, arrancamos):
         self.__enmarcha=arrancamos
+
         if self.__enmarcha:
+            chequeo=self.__chequeoInterno()
+        if self.__enmarcha and chequeo:
             return "El coche esta en marcha"
+        elif self.__enmarcha and chequeo == False:
+            return "Algo ha ido mal en el chequeo. No se puede arrancar "
         else:
             return "El coche esta parado"
 
     def estado(self):
         print("El coche tiene",self.__ruedas,"ruedas. Un ancho de", self.__anchoChasis,"y un largo de",
               self.__largoChasis)
+
+    # Para encapsular un metodo pues es igual que con una variables con __
+    def __chequeoInterno(self):
+        print("Realizando chequeo interno")
+
+        #Aqui creo variables dentro del metodo del objeto
+        self.gasolina = "ok"
+        self.aceite="ok"
+        self.puertas="cerradas"
+
+        if self.gasolina == "ok" and self.aceite == "ok" and self.puertas=="cerradas":
+            return True
+        else:
+            return False
 
 
 #   ---------------- CODIGO --------------
@@ -32,6 +51,7 @@ print("\n---------- A continucacion creamos el segundo objeto ----------\n")
 
 miCoche2=Coche()
 print(miCoche2.arrancar(False))
+
 # Esta variable al estar encapsulada no tiene nada que ver, pero no falla, ya se vera por que
 miCoche2.ruedas=2
 
