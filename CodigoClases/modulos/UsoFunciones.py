c5f81eafdbe23bc3151eb480999cba08cde817fd
GabiMSI <gabigarciagar@gmail.com>
2020-06-28 19:22:31 +0200

    mas codigo

---
+++ CodigoClases/modulos/UsoFunciones.py
@@ -0,0 +1,11 @@
+#Asi hay que usar las funciones como FuncionesMatematicas.sumar(2,4)
+# import FuncionesMatematicas
+
+#Asi solo nos importa sumar
+# from FuncionesMatematicas import sumar
+
+from FuncionesMatematicas import *
+
+sumar(2,4)
+restar(9,6)
+multiplicar(2,3)
---
+++ CodigoClases/modulos/UsoVehiculos.py
@@ -0,0 +1,4 @@
+from Vehiculos import *
+
+miCoche=Vehiculo("Mazda","Mx5")
+miCoche.estado()
