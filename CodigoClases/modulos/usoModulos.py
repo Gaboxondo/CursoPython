a567b8441f0b2b830cd7314ed024509f55d5b342
GabiMSI <gabigarciagar@gmail.com>
2020-06-28 19:49:26 +0200

    estaba mal el codigo

---
+++ CodigoClases/modulos/usoModulos.py
@@ -0,0 +1,7 @@
+from calculos.CalculosGenerales import dividir
+from calculos.basicos.operacionesBasicas import sumar
+from calculos.avanzadas.avanzadas import *
+
+dividir(10,2)
+sumar(2,5)
+potencia(2,4)
---
+++ CodigoClases/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="paquetecalculos",
+    version="1.0",
+    description="Paquete de redondeo y potencia",
+    author="Gabi",
+    author_email="gabigarciagar@gmail.com",
+    url="www.nohay.com",
+
+    packages=["calculos", "calculos.avanzadas"]
+)
